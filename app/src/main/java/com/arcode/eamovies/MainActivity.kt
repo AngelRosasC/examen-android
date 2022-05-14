9e258dbda874b56058f22aa8deb5509a7acb6e68
Angel Rosas <marosascabrera@gmail.com>
2022-05-14 11:57:18 -0500

    Initial commit.

---
+++ app/src/main/java/com/arcode/eamovies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.arcode.eamovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
