68703c986c2c955dc914ddac4aa274612559a910
Angel Rosas <marosascabrera@gmail.com>
2022-05-19 00:35:29 -0500

    Configuring the onBackPressed button to exit the application.

--- app/src/main/java/com/arcode/eamovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/arcode/eamovies/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.arcode.eamovies.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.arcode.eamovies.R
@@ -11,13 +12,28 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var backPressedTime: Long = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_EAMovies)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        NavigationUI.setupWithNavController(binding.mainBottomNavigation, navHostFragment.navController)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.mainBottomNavigation,
+            navHostFragment.navController)
+    }
+
+    override fun onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            super.onBackPressed()
+            finish()
+            return
+        } else {
+            Toast.makeText(this, getString(R.string.main_back_press), Toast.LENGTH_SHORT).show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }
