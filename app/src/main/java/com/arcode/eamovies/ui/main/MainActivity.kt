6d7cba277f4761c378af0cfb67a5f4ad74a2caca
Angel Rosas <marosascabrera@gmail.com>
2022-05-19 00:11:35 -0500

    Configuring navigation in BottonNavigation.

--- app/src/main/java/com/arcode/eamovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/arcode/eamovies/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.arcode.eamovies.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.arcode.eamovies.R
 import com.arcode.eamovies.databinding.ActivityMainBinding
 
@@ -14,5 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.mainBottomNavigation, navHostFragment.navController)
     }
 }
